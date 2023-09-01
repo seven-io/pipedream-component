e6c87b682f6799e21d7436170afcdecb20c9ca7a
André Matthies <matthiez@gmail.com>
2023-09-01 14:02:09 +0200

    add build step

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+pd publish './actions/lookup-cnam/lookup-cnam.mjs'
+pd publish './actions/lookup-format/lookup-format.mjs'
+pd publish './actions/lookup-hlr/lookup-hlr.mjs'
+pd publish './actions/lookup-mnp/lookup-mnp.mjs'
+pd publish './actions/send-sms/send-sms.mjs'
+pd publish './actions/tts-call/tts-call.mjs'
